4f943ff239dd650b42d9b11319d9d2395d57657f
Jonathan Phan <jonathan.phan804@gmail.com>
2022-04-13 15:59:44 -0700

    updated models and requirements

--- game/models.py
+++ game/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Square_Model(models.Model):
+    square = { #0 for O, 1 for X, -1 for none
+        "topLeft" : -1,
+        "topMid" : -1,
+        "topRight" : -1,
+        "midLeft" : -1,
+        "midMid" : -1,
+        "midRight" : -1,
+        "botLeft" : -1,
+        "botMid" : -1,
+        "botRight" : -1
+    }
+    def __str__(self):
+        return """{topLeft} | {topMid} | {topRight}\n 
+                  ------------\n
+                  {midLeft} | {midMid} | {midRight}\n
+                  ------------\n
+                  {botLeft} | {botMid} | {botRight}""".format(**self.square)
+    
