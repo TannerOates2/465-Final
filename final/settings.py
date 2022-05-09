8f8a7ae33a36fb60fb31b8b835787f79d6f4b98a
Tanner Oates <tanner.oates97@gmail.com>
2022-05-08 21:12:56 -0700

    requirements

--- final/settings.py
+++ final/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home"
 LOGIN_URL = ''
+
+django_heroku.settings(locals())
