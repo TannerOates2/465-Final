31ff1a29236f117eaab157dc787bd952307d5594
Tanner Oates <tanner.oates97@gmail.com>
2022-05-10 15:42:32 -0700

    procfile

--- final/asgi.py
+++ final/asgi.py
@@ -1,16 +1,21 @@
 import os
-
-from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-import game.routing
-
+from django.conf.urls import url
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final.settings')
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
+
+from channels.auth import AuthMiddlewareStack
+import game.routing
+
+
+
 # application = get_asgi_application()
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             game.routing.websocket_urlpatterns
